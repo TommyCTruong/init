84c52ac1b15766919878499f501ad697a49e2a61
Tam C Truong <tam@cuti.net>
2022-09-21 12:58:31 -0500

    chore: savepoint

--- init.sh
+++ init.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-read -p 'Enter password: ' pass
+#read -p 'Enter password: ' pass
 
-openssl enc -in id.tar.gz.enc -out id.tar.gz -d -aes-256-cbc -salt -pass "pass:${pass}" -pbkdf2
-
-tar xvfz id.tar.gz
+gpg -d keys.tar.gz.gpg
+#kopenssl enc -in id.tar.gz.enc -out id.tar.gz -d -aes-256-cbc -salt -pass "pass:${pass}" -pbkdf2
+sleep 2
+tar xvfz keys.tar.gz
 
 sleep 2
 
-cp id_r* ~/.ssh
-rm id.tar.gz id_r*
-sudo pacman -S openssh chezmoi --needed --noconfirm
-sudo systemctl --now enable sshd
+#cp id_r* ~/.ssh
+#rm id.tar.gz id_r*
+#sudo pacman -S openssh chezmoi --needed --noconfirm
+#sudo systemctl --now enable sshd
 
-chezmoi init --apply git@github.com:tommyctruong/dotfiles.git
+#chezmoi init --apply git@github.com:tommyctruong/dotfiles.git
 
-sleep 10
+#sleep 10
 
-~/.zsh/scripts/install_pacman.sh
-###
+#~/.zsh/scripts/install_pacman.sh
